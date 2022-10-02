1444a702adb5a7dfc662fdfcaa63c77fc9faeafd
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-02 08:54:40 -0700

    Adding styles to Rest-Rant

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,24 +1,24 @@
-const React = require('react')
-const Def = require('../default')
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-function index (data) {
-    let placesFormatted = data.places.map((place) => {
-      return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
-        </div>
-      )
-    })
-    return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX PAGE</h1>
-              {placesFormatted}
-          </main>
-      </Def>
-  )
-  }
-  
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-module.exports = index
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+// Listen for Connections
+app.listen(process.env.PORT)
