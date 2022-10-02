ec425a2d5da13766bf303b7ad4cbfd1177c697c5
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-02 12:28:21 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'images/place1.jpg/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'images/place2.jpg/250/250'
+      }]
