ef557f2dfe4733a390fac84d19ee34f3f73532ba
Royce Childers <105823118+Rollsroyce95@users.noreply.github.com>
2022-10-02 13:55:07 -0700

    update syntax errors

--- controllers/places.js
+++ controllers/places.js
@@ -2,10 +2,10 @@ const router = require('express').Router()
 const places = require("../models/places")
 
 router.post('/', (req, res) => {
+    console.log(req.body)
   
     if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
+        req.body.pic = '/images/place3.jpg'
     }
     if (!req.body.city) {
         req.body.city = 'Anytown'
